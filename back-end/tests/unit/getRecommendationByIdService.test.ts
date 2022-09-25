51f3018e3eaddcd06022828dc2919b987d33c631
Thalya Busato Beltrame <beltrame.thalya@gmail.com>
2022-09-25 09:40:05 -0300

    refactor: unit test for recommendation fetching service by id

--- back-end/tests/unit/getRecommendationByIdService.test.ts
+++ back-end/tests/unit/getRecommendationByIdService.test.ts
@@ -1,13 +1,10 @@
 import { jest } from '@jest/globals';
-import { Recommendation } from '@prisma/client';
 
 import { recommendationRepository } from '../../src/repositories/recommendationRepository';
 import { recommendationService } from '../../src/services/recommendationsService';
 import { recommendationFactory } from '../factories/recommendationFactory';
 
 beforeEach(() => {
-  jest.restoreAllMocks();
-  jest.clearAllMocks();
   jest.resetAllMocks();
 });
 
@@ -15,23 +12,35 @@ describe('getById', () => {
   const newRecommendation =
     recommendationFactory.generateRecommendationRequest();
 
+  const recommendationResponse = {
+    id: 1,
+    ...newRecommendation,
+    score: 0,
+  };
+
   it('Should return the recommendation', async () => {
     jest
       .spyOn(recommendationRepository, 'find')
-      .mockResolvedValueOnce(newRecommendation as Recommendation);
+      .mockResolvedValueOnce(recommendationResponse);
 
-    const result = await recommendationService.getById(1);
+    const result = await recommendationService.getById(
+      recommendationResponse.id
+    );
 
-    expect(result).toEqual(newRecommendation);
-    expect(recommendationRepository.find).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(recommendationResponse);
+    expect(recommendationRepository.find).toHaveBeenCalledWith(
+      recommendationResponse.id
+    );
   });
 
   it('Should throw an error if the recommendation does not exist', async () => {
     jest.spyOn(recommendationRepository, 'find').mockResolvedValueOnce(null);
 
-    const result = recommendationService.getById(1);
+    const result = recommendationService.getById(recommendationResponse.id);
 
     expect(result).rejects.toEqual({ type: 'not_found', message: '' });
-    expect(recommendationRepository.find).toHaveBeenCalledTimes(1);
+    expect(recommendationRepository.find).toHaveBeenCalledWith(
+      recommendationResponse.id
+    );
   });
 });
